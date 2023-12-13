8f443ba6a4873a0c97efbe81accc9750e610e994
Rodrigo Andreta <136493387+RodrigoAndret@users.noreply.github.com>
2023-12-13 12:38:16 -0300

    Add files via upload

--- demo/src/main/java/com/projeto/controller/Controlador.java
+++ demo/src/main/java/com/projeto/controller/Controlador.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,20 +32,21 @@ public ResponseEntity<Docente> criarDocente(@RequestBody Docente docente) {
 }
 
 @GetMapping("/docentes/{id}")
-public String detalharDocente(@PathVariable("id") @RequestParam Long id) {
-	Docente docente = new Docente(id);
-	String nome = docente.getNome();	
-	return "Nome: " + nome + "/ ID: " + id;	
+public ResponseEntity<Docente> detalharDocente(@PathVariable("id") Long id) {
+	Docente docente = docenteService.detalharDocente(id);	
+	return new ResponseEntity<>(docente,HttpStatus.OK);
 }
 
-@PostMapping("/docentes/{id}")
-public void alterarDocente(@PathVariable("id") @RequestParam Long id) {
-	docenteService.alterarDocente(id);
+@PutMapping("/alterarDocentes/{id}")
+public ResponseEntity<Docente> alterarDocente(@PathVariable("id") Long id, @RequestBody Docente docente) {
+	docente = docenteService.alterarDocente(id,docente);
+	return new ResponseEntity<>(docente,HttpStatus.CREATED);
 }
 
-@PostMapping("/docentes/{id}")
-public void deletarDocente(@PathVariable("id") @RequestParam Long id) {
+@DeleteMapping("/deletarDocentes/{id}")
+public ResponseEntity<?> deletarDocente(@PathVariable("id") Long id) {
 	docenteService.deletarDocente(id);	
+	return new ResponseEntity<>("Docente deletado com sucesso!",HttpStatus.NO_CONTENT);
 }
 
 
--- demo/src/main/java/com/projeto/service/DocenteService.java
+++ demo/src/main/java/com/projeto/service/DocenteService.java
@@ -15,13 +15,32 @@ public class DocenteService {
     public Docente criarDocente(Docente docente) {
         return docenteRepository.save(docente);
     }
-    
-    public Docente alterarDocente(Docente docente) {
-
+        
+    public Docente detalharDocente(Long id) {
+    	return docenteRepository.findById(id).orElseThrow(() -> new Error());
     }
     
-    public Docente deletarDocente(Docente docente) {
-        return docenteRepository.deleteById(docente);
+    public Docente alterarDocente(Long id, Docente docente) {
+    	Docente docenteAntigo = docenteRepository.findById(id).orElseThrow(() -> new Error());
+		
+    	if (docente.getAulas() != null || !docente.getAulas().isEmpty()) {
+			docenteAntigo.setAulas(docente.getAulas());
+		}
+		
+		if (docente.getProcedimentos() != null || !docente.getProcedimentos().isEmpty()) {
+			docenteAntigo.setProcedimentos(docente.getProcedimentos());
+		}
+		
+		if (docente.getNome() != null || !docente.getNome().isEmpty()) {
+			docenteAntigo.setNome(docente.getNome());
+		}
+				   	
+    	return docenteRepository.save(docenteAntigo);
+    }
+    
+    public void deletarDocente(Long id) {  	 
+    	Docente docente = docenteRepository.findById(id).orElseThrow(() -> new Error());
+    	docenteRepository.delete(docente);    	
     }
     
     
